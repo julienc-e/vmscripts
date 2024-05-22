2134f0b0a269e8c8f1cd56f92bb1c95f0e074776
julienc-e <71256460+julienc-e@users.noreply.github.com>
2024-05-22 11:12:27 +0200

    fix(grademe.js): Removing Additional logs

--- intra/grademe.js
+++ intra/grademe.js
@@ -9,13 +9,10 @@
 // ==/UserScript==
 
 const targetNode = document.getElementsByClassName('title');
-console.log(targetNode)
 
 for (item of targetNode) {
   if (item.title != "") {
-    console.log(item.title);
     let currURIParsed = document.URL.split("/");
-    console.log(currURIParsed);
     let uri = currURIParsed[0] + "//" + currURIParsed[2] + "/"+ currURIParsed[3] + "/" + currURIParsed[4] + "/" + currURIParsed[5] + "/" + currURIParsed[6] + "/" + currURIParsed[7] + "/bareme/" + item.title;
     var zNode = document.createElement ('div');
     zNode.innerHTML = `<button id="myButton" type="button" onclick="window.open('${uri}')">` +
